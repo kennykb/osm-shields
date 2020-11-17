0143d656ad875f350fe268891e10fc9e7cb0b8f8
Kevin Kenny <kevin.b.kenny@gmail.com>
2020-11-16 21:55:17 -0500

    Another tranche of changes moving toward minutely updates.
    Add a --prefix option to the utility Tcl scripts, to allow overriding
    config.tcl when testing on a smaller extract.
    
    Separate out creation of indices on the shield tables.
    
    Make shields only for route relations that the program hasn't seen before.
    (MAJOR performance win - updates run in an eyeblink rather than minutes.)
    
    Handle modifications to route relations. (Lua script was causing the
    same relation members to appear 2-3 times; see
    https://github.com/openstreetmap/osm2pgsql/issues/1325
    for further details.) Still can't get deletion of route relations right.

--- shieldtables.lua
+++ shieldtables.lua
@@ -61,13 +61,13 @@ end
 
 function ShieldTablesMethods:process_way(object)
 
-    if self.osm2pgsql.stage == 2 then
+    if self.osm2pgsql.stage == 2 and object.tags.highway then
         local d = self.w2routes[object.id]
         if d then
-            if object.tags.highway then
-                for _, row in ipairs(d) do
-                    self.shieldway_tbl:add_row({
-		        relid = row.relid,
+	    for relid, e in pairs(d) do
+	        for _, row in ipairs(e) do
+		    self.shieldway_tbl:add_row({
+		        relid = relid,
 		        idx = row.idx,
 		        wayid = object.id,
 		        role = row.role,
@@ -75,7 +75,9 @@ function ShieldTablesMethods:process_way(object)
 	        end
 	    end
 	end
-	-- It is tempting to return at this point, but osm2pgsql will have
+	
+	-- It is tempting to return all the way to the caller in
+	-- osm2pgsql at this point, but osm2pgsql will have
 	-- deleted from the database any rows that are scheduled for 
 	-- reprocessing. They need to be reinserted, so continue recreating
 	-- the identical rows that will have existed in phase 1.
@@ -99,30 +101,54 @@ function ShieldTablesMethods:process_relation(object)
 	    route = object.tags.route,
 	    network = object.tags.network,
 	    ref = object.tags.ref,
+	    changed = true,
 	})
+
+	-- If this is the second time that we have seen a relation,
+	-- then the first one was a delete, and its ways need to be
+	-- deleted from r2ways and w2routes
+
+	ways = self.r2ways[object.id]
+	if ways then
+	    for w, _ in pairs(ways) do
+	    	self.w2routes[w][object.id] = nil
+	    end
+	    self.r2ways[object.id] = nil
+	end
+
         for idx, member in ipairs(object.members) do
 
 	    -- go through the route members for ones with appropriate roles
 
             if self:is_route_member(member) then
 
-		-- record a route member in self.w2routes
-		
+		-- record a route member in self.w2routes and self.r2ways
+
 		local d = self.w2routes[member.ref]
 		if not d then
 		    d = {}
 		end
-                d[#d+1] = {
-                    relid = object.id,
+		local d2 = d[object.id]
+		if not d2 then
+		    d2 = {}
+		end
+                d2[#d2+1] = {
                     idx = idx,
                     role = member.role,
                 }
+		d[object.id] = d2
 		self.w2routes[member.ref] = d
-		
+
+		d = self.r2ways[object.id]
+		if not d then
+		    d = {}
+		end
+		d[member.ref] = true
+		self.r2ways[object.id] = d
+
             end
         end
     end
-
 end
 
 -- The 'select_relation_members' method, given a relation, returns
@@ -155,6 +181,7 @@ shieldtables.new = function(prefix)
     local values = {
 
         osm2pgsql = osm2pgsql,
+	-- print = print,
 
         -- The 'shieldroute' table has one row per route that may be marked
 	-- with a shield. The columns are:
@@ -162,6 +189,9 @@ shieldtables.new = function(prefix)
 	--     route   - Value of the 'route' key of the relation
 	--     network - Value of the 'network' key of the relation
 	--     ref     - Value of the 'ref' key of the relation
+	--     changed - 1 if the relation (not its members) has changed
+	--               since last being examined. Used to trigger generation
+	--               of graphichs files for shields.
 
         shieldroute_tbl = osm2pgsql.define_table{
 	    name = prefix .. '_shieldroute',
@@ -170,6 +200,7 @@ shieldtables.new = function(prefix)
 		{ column = 'network', type = 'text' },
 	        { column = 'route', type = 'text' },
 		{ column = 'ref', type = 'text' },
+		{ column = 'changed', type = 'boolean' },
 	    }
 	},
 
@@ -190,15 +221,17 @@ shieldtables.new = function(prefix)
 	    }
 	},
 
-	-- w2routes is a table. The keys are OSM way IDs for ways
-	-- that may require shields.
-	-- Each value is a table of tables. Each table in the list has
-	-- the values:
-	--     relid - OSM ID of the route relation
-	--     idx   - Ordinal position of the way within the route relation
-	--     role  - Role of the way within the route relation
+	-- w2routes[wayID][relationID] consists of a list of pairs,
+	-- (idx = N, role = R)
+	-- if the way identified by wayID is a member of the route relation
+	-- relationID, at index N, with role R
 
 	w2routes = {},
+
+	-- r2ways[relationID][wayID] is true if the way identified by
+	-- wayID is a member of the relation identified by relationID.
+
+	r2ways = {},
 
     }
 
